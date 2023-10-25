0147efe8645a95d875a3495a32dba2a269067201
ztao-8 <ztao8@dons.usfca.edu>
2023-10-24 22:52:36 -0700

    nextguess

--- src/CleverAIPlayer.java
+++ src/CleverAIPlayer.java
@@ -13,10 +13,9 @@ public class CleverAIPlayer implements WheelOfFortunePlayer {
 
     @Override
     public char nextGuess() {
-        Random rand = new Random();
-        int num = rand.nextInt(guesslist.length()); // gets 0, 1, or 2
-        char guess = guesslist.charAt(num);
-        guesslist.deleteCharAt(num);
+        // gets 0, 1, or 2
+        char guess = guesslist.charAt(0);
+        guesslist.deleteCharAt(0);
         return guess;
     }
 
