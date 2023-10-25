65cd5b646b47a70e5d915ef3b62e54e821cc4548
ztao-8 <ztao8@dons.usfca.edu>
2023-10-24 21:13:30 -0700

    change reset, data member

--- src/DefaultAIPlayer.java
+++ src/DefaultAIPlayer.java
@@ -1,16 +1,28 @@
+import java.util.Random;
+
 public class DefaultAIPlayer implements WheelOfFortunePlayer{
     private StringBuilder guesslist = new StringBuilder("abcdefghijklmnopqrstuvwxyz");
+    protected int score;
+
 
     @Override
     public String playerId() {
         return "Default AI player";
     }
 
+
+    @Override
+    public char nextGuess() {
+        Random rand = new Random();
+        int num = rand.nextInt(guesslist.length()); // gets 0, 1, or 2
+        char guess = guesslist.charAt(num);
+        guesslist.deleteCharAt(num);
+        return guess;
+    }
+
     @Override
     public void reset() {
-        phrase = WheelOfFurtune.randomPhrase();
-        secret = new StringBuilder(getHiddenPhrase());
         score = 26;
-        System.out.println(secret);
+        guesslist =  new StringBuilder("abcdefghijklmnopqrstuvwxyz");
     }
 }
